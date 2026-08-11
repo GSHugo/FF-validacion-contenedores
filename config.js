db223db6a766fb284c31c2b82c5d64f0cadc049f
GSHugo <vto.garciasanchez98@gmail.com>
2026-08-10 23:48:45 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Pega aquí la URL /exec de la implementación de Apps Script.
+window.APP_CONFIG = {
+  API_URL: 'https://script.google.com/macros/s/AKfycbzp7k_oo_yFz_yP99JK5Uq4-WvezHLcw-TIplIv-1v6y4tZfJL0EfHmUMCBimGpXCTL/exec',
+  VERSION: '2.0.1',
+  REFRESH_MS: 2500
+};
