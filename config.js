3c155fb95e3c2b15a6a8bd644858393bb91e6f65
GSHugo <vto.garciasanchez98@gmail.com>
2026-08-14 08:01:30 -0600

    Update API_URL and VERSION in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Pega aquí la URL /exec de la implementación de Apps Script.
 window.APP_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbyI9HByGTd_NpP1V9rpw_hsncjbzrz895RaLsFeOLdpb9ruDFnT7rpgTfmiil0b6XQ3/exec',
-  VERSION: '2.5.0',
+  API_URL: 'https://script.google.com/macros/s/AKfycbw2_FCv9nGBRmmMdRo4PUJhUvEIWCNVGHYX4KPo1jtoLJkDY4glkYtxKoMLr1PDMjEW/exec',
+  VERSION: '2.6.0',
   REFRESH_MS: 5000
 };
