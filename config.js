611714bd8585e732faaeb829b670f1252b0578ce
GSHugo <vto.garciasanchez98@gmail.com>
2026-08-18 10:48:43 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Pega aquí la URL /exec de la implementación de Apps Script.
 window.APP_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbw2_FCv9nGBRmmMdRo4PUJhUvEIWCNVGHYX4KPo1jtoLJkDY4glkYtxKoMLr1PDMjEW/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwzdjtqYrlePk3LBcdnAL2mUj4avpyb4vFmNHCQwlp-Jweiww58nG0NLPxMI4D4Tr90/exec',
   VERSION: '2.6.0',
   REFRESH_MS: 5000
 };
